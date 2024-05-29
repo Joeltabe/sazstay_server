ebe76ecbf7e555e6a3271298ef2a43cb92a8bcf6
Joeltabe <joeltabe3@gmail.com>
2024-05-29 03:53:10 +0100

    propertys

--- server.js
+++ server.js
@@ -22,8 +22,62 @@ connection.connect(err => {
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Example route to get data from MySQL
+// Endpoint to get the latest properties or search results
+app.post('/api/properties', (req, res) => {
+  const { location, type, offer, bhk, min, max, status, furnished } = req.body;
+  
+  let query = "SELECT * FROM property";
+  let queryParams = [];
+  
+  if (location || type || offer || bhk || min || max || status || furnished) {
+    query += " WHERE";
+    const conditions = [];
+    
+    if (location) {
+      conditions.push(" address LIKE ?");
+      queryParams.push(`%${location}%`);
+    }
+    if (type) {
+      conditions.push(" type LIKE ?");
+      queryParams.push(`%${type}%`);
+    }
+    if (offer) {
+      conditions.push(" offer LIKE ?");
+      queryParams.push(`%${offer}%`);
+    }
+    if (bhk) {
+      conditions.push(" bhk LIKE ?");
+      queryParams.push(`%${bhk}%`);
+    }
+    if (status) {
+      conditions.push(" status LIKE ?");
+      queryParams.push(`%${status}%`);
+    }
+    if (furnished) {
+      conditions.push(" furnished LIKE ?");
+      queryParams.push(`%${furnished}%`);
+    }
+    if (min && max) {
+      conditions.push(" price BETWEEN ? AND ?");
+      queryParams.push(min, max);
+    }
+    
+    query += conditions.join(" AND");
+  }
+  
+  query += " ORDER BY date DESC";
+  
+  connection.query(query, queryParams, (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+// Example route to get users
 app.get('/api/users', (req, res) => {
   connection.query('SELECT * FROM users', (err, results) => {
     if (err) {
